074a4e67e8a7813cb98a27891cb171af1364f47f
Enokek <enok.ekelund@gmail.com>
2022-09-15 10:21:04 +0200

    Structure in place, ready to go

---
+++ excercises.js
@@ -0,0 +1,8 @@
+console.log("I am Alive");
+
+function exercise1() {
+  console.log("I am Alive");
+  var textbox1value = document.getEelementById("txt1").value;
+  var textbox2value = document.getEelementById("txt2").value;
+  console.log(`${textbox2value}, ${textbox1value} ${textbox1value.value}`);
+}
